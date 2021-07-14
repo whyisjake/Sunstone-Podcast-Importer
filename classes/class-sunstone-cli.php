604c9defadc6bc5d24fffe08249c8f5739d7a0da
Jake Spurlock <whyisjake@gmail.com>
2021-07-13 18:43:37 -0700

    Rewrite a few things.

--- classes/class-sunstone-cli.php
+++ classes/class-sunstone-cli.php
@@ -12,16 +12,16 @@ class Sunstone_CLI extends WP_CLI_Command {
 	 * Create archive posts
 	 * If you run `wp sunstone install --force`, it will bybass the installed check, and force the install.
 	 *
-	 * @subcommand 	install
-	 * @param  		[type] $args       No args at the moment.
-	 * @param  		[type] $assoc_args --force
-	 * @return 		string             CLI Output.
+	 * @subcommand install
+	 * @param     [type] $args       No args at the moment.
+	 * @param     [type] $assoc_args --force
+	 * @return    string             CLI Output.
 	 */
 	public function install( $args, $assoc_args ) {
 
-		$force = isset( $assoc_args['force'] );
-		$default = false;
-		$option = 'have_archive_posts_been_added';
+		$force     = isset( $assoc_args['force'] );
+		$default   = false;
+		$option    = 'have_archive_posts_been_added';
 		$installed = get_option( $option, $default );
 
 		WP_CLI::out( 'Have the posts been added? ' );
@@ -33,13 +33,12 @@ class Sunstone_CLI extends WP_CLI_Command {
 
 		WP_CLI::line();
 
-
 		// Have we intalled already?
 		if ( ! $installed or $force ) {
 
 			// Kick off the class.
-			$Sunstone_Posts_Importer = new Sunstone_Posts_Importer();
-			$Sunstone_Posts_Importer->install();
+			$sunstone_posts_importer = new sunstone_posts_importer();
+			$sunstone_posts_importer->install();
 
 			// Update the option
 			update_option( $option, true );
@@ -51,16 +50,16 @@ class Sunstone_CLI extends WP_CLI_Command {
 	 * Create archive posts
 	 * If you run `wp sunstone install --force`, it will bybass the installed check, and force the install.
 	 *
-	 * @subcommand 	words
-	 * @param  		[type] $args       No args at the moment.
-	 * @param  		[type] $assoc_args --force
-	 * @return 		string             CLI Output.
+	 * @subcommand words
+	 * @param      [type] $args       No args at the moment.
+	 * @param      [type] $assoc_args --force
+	 * @return     string             CLI Output.
 	 */
 	public function words( $args, $assoc_args ) {
 
 		// Kick off the class.
-		$Sunstone_Posts_Importer = new Sunstone_Posts_Importer();
-		$Sunstone_Posts_Importer->words( $args, $assoc_args );
+		$sunstone_posts_importer = new sunstone_posts_importer();
+		$sunstone_posts_importer->words( $args, $assoc_args );
 
 	}
 
@@ -68,16 +67,16 @@ class Sunstone_CLI extends WP_CLI_Command {
 	 * Create archive posts
 	 * If you run `wp sunstone install --force`, it will bybass the installed check, and force the install.
 	 *
-	 * @subcommand 	events
-	 * @param  		[type] $args       No args at the moment.
-	 * @param  		[type] $assoc_args --force
-	 * @return 		string             CLI Output.
+	 * @subcommand events
+	 * @param      [type] $args       No args at the moment.
+	 * @param      [type] $assoc_args --force
+	 * @return     string             CLI Output.
 	 */
 	public function events( $args, $assoc_args ) {
 
 		// Kick off the class.
-		$Sunstone_Posts_Importer = new Sunstone_Posts_Importer();
-		$Sunstone_Posts_Importer->events( $args, $assoc_args );
+		$sunstone_posts_importer = new sunstone_posts_importer();
+		$sunstone_posts_importer->events( $args, $assoc_args );
 
 	}
 
@@ -85,16 +84,16 @@ class Sunstone_CLI extends WP_CLI_Command {
 	 * Create archive posts
 	 * If you run `wp sunstone install --force`, it will bybass the installed check, and force the install.
 	 *
-	 * @subcommand 	speakers
-	 * @param  		[type] $args       No args at the moment.
-	 * @param  		[type] $assoc_args --force
-	 * @return 		string             CLI Output.
+	 * @subcommand speakers
+	 * @param      [type] $args       No args at the moment.
+	 * @param      [type] $assoc_args --force
+	 * @return     string             CLI Output.
 	 */
 	public function speakers( $args, $assoc_args ) {
 
 		// Kick off the class.
-		$Sunstone_Posts_Importer = new Sunstone_Posts_Importer();
-		$Sunstone_Posts_Importer->speakers( $args, $assoc_args );
+		$sunstone_posts_importer = new sunstone_posts_importer();
+		$sunstone_posts_importer->speakers( $args, $assoc_args );
 
 	}
 
